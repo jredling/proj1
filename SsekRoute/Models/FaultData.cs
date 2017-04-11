c3469ffb3c0021feff3856cdb96ead0e00adc754
Joel Wahlund <joel.wahlund@r2m.se>
2017-04-11 14:41:33 +0200

    first draft added ssek models

---
+++ SsekRoute/Models/FaultData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace SsekRoute.Models
+{
+    public class FaultData
+    {
+        public string FaultingMessage { get; set; }
+        public string TxId { get; set; }
+        public List<FaultItems> FaultItems { get; set; }
+    }
+
+    public class FaultItems
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public string System { get; set; }
+    }
+}
