1ee81e1e63bf07c8d7053b7aebc6d142b503f567
Joel Wahlund <joel.wahlund@r2m.se>
2017-04-13 14:27:06 +0200

    updated api post method

--- Api.Receiver/Controllers/SsekController.cs
+++ Api.Receiver/Controllers/SsekController.cs
@@ -1,25 +1,40 @@
 ﻿using Api.Receiver.Models;
+using SsekRoute.Domain.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Web.Http;
+using System.Xml;
 using System.Xml.Serialization;
+using SsekRoute.Factory;
 
 namespace Api.Receiver.Controllers
 {
     public class SsekController : ApiController
     {
-        // POST api/values
-        //public IHttpActionResult Post([FromBody]ParamsObject paramsObject)
-        //{
-        //    return Ok(paramsObject);
-        //}
+        private ISSEKFactory _iFactory;
+
+        public SsekController(ISSEKFactory iFactory)
+        {
+            _iFactory = iFactory;
+        }
+
+        public SsekController() : this(new SSEKFactory()) { }
+
         public IHttpActionResult Post([FromBody]string data)
         {
+            var transaction = new Transaction();
 
-            return Ok(data);
+            transaction = _iFactory.CreateObject(data);
+                        
+            return Ok(transaction);
+
+            // försök skicka till service, returnera ett Receipt objekt med resultat?
         }
     }
 }
